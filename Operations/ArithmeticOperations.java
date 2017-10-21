3aac613d4b1b57a37c6976fe48df39a6f0db5710
Erik Laucks <phisigmapi314@gmail.com>
2017-10-21 02:08:50 -0400

    Added CPU flags

--- Operations/ArithmeticOperations.java
+++ Operations/ArithmeticOperations.java
@@ -1,6 +1,11 @@
 package Operations;
 
 public class ArithmeticOperations {
+    private static boolean flagZ = false;
+    private static boolean flagN = false;
+    private static boolean flagC = false;
+    private static boolean flagV = false;
+
     public static char[] convertString(String s) {
         char[] num = new char[32];
         for (int i = 0; i < 32; i++) num[i] = '0';
@@ -50,6 +55,23 @@ public class ArithmeticOperations {
                 carry = '1';
             }
         }
+
+        int numOnes = 0;
+        for (int i = 0; i < 32; i++) {
+            if (num[i] == '1') numOnes++;
+        }
+
+        if (numOnes == 0) flagZ = true;
+        else flagZ = false;
+
+        if (num[0] == '1') flagN = true;
+        else flagN = false;
+
+        if (carry == '1') flagC = true;
+        else flagC = false;
+
+        if ((one[0] == '0' && two[0] == '0' && num[0] == '1') || (one[0] == '1' && two[0] == '1' && num[0] == '0')) flagV = true;
+        else flagV = false;
 
         return num;
     }
