1a00dbf444027975385ca490f11adda423d883e0
Erik Laucks <phisigmapi314@gmail.com>
2017-10-22 15:42:25 -0400

    Finished GUI

--- Simulator/SimulatorGUI.java
+++ Simulator/SimulatorGUI.java
@@ -1,12 +1,15 @@
 package Simulator;
 
+import Instructions.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 public class SimulatorGUI {
     private Simulator simulator;
     private Frame stateFrame;
     private Frame memFrame;
+    private Timer timer;
 
     public SimulatorGUI(Simulator s) {
         this.simulator = s;
@@ -18,14 +21,24 @@ public class SimulatorGUI {
         this.stateFrame.setSize(800,600);
         this.stateFrame.setLayout(null);
         this.stateFrame.setVisible(true);
+
+        this.memFrame = new Frame("Memory State");
+        this.memFrame.addWindowListener(new WindowAdapter() {
+            @Override public void windowClosing(WindowEvent we) { System.exit(0); }
+        });
+        this.memFrame.setSize(400,800);
+        this.memFrame.setLayout(null);
+        this.memFrame.setVisible(true);
+
+        this.timer = null;
     }
 
-    public void draw(Byte[] memory, char[][] registers, int pc, String instruction, boolean[] flags) {
+    public void draw(Byte[] memory, char[][] registers, int pc, Instruction instruction, boolean[] flags) {
         this.showState(registers, pc, instruction, flags);
         this.showMemory(memory);
     }
 
-    public void showState(char[][] registers, int pc, String instruction, boolean[] flags) {
+    private void showState(char[][] registers, int pc, Instruction instruction, boolean[] flags) {
         this.stateFrame.removeAll();
 
         Label regLabel = new Label("Registers:");
@@ -42,7 +55,7 @@ public class SimulatorGUI {
         registerDisplay.setBounds(10,40, 450,300);
         this.stateFrame.add(registerDisplay);
 
-        Label pcLabel = new Label("Program Counter: " + Integer.toHexString(pc) + " (" + pc + ")");
+        Label pcLabel = new Label("Program Counter: 0x" + Integer.toHexString(pc) + " (" + pc + ")");
         pcLabel.setBounds(470,10, 200,20);
         this.stateFrame.add(pcLabel);
 
@@ -50,41 +63,152 @@ public class SimulatorGUI {
         instructionHeaderLabel.setBounds(470,40, 320,15);
         this.stateFrame.add(instructionHeaderLabel);
 
-        Label instructionLabel = new Label(instruction);
+        Label instructionLabel = new Label(instruction.getBinary());
         instructionLabel.setBounds(470,55, 320,15);
         this.stateFrame.add(instructionLabel);
 
+        Label instructionPlainLabel = new Label(instruction.getPlain());
+        instructionPlainLabel.setBounds(470,70, 320,15);
+        this.stateFrame.add(instructionPlainLabel);
+
         Label flagHeaderLabel = new Label("Flags:");
-        flagHeaderLabel.setBounds(470,80, 320,15);
+        flagHeaderLabel.setBounds(470,90, 320,15);
         this.stateFrame.add(flagHeaderLabel);
 
         Label flagZLabel = new Label("Z: " + (flags[0] ? "1" : "0"));
-        flagZLabel.setBounds(470,95, 320,15);
+        flagZLabel.setBounds(470,105, 320,15);
         this.stateFrame.add(flagZLabel);
 
         Label flagNLabel = new Label("N: " + (flags[1] ? "1" : "0"));
-        flagNLabel.setBounds(470,110, 320,15);
+        flagNLabel.setBounds(470,120, 320,15);
         this.stateFrame.add(flagNLabel);
 
         Label flagCLabel = new Label("C: " + (flags[2] ? "1" : "0"));
-        flagCLabel.setBounds(470,125, 320,15);
+        flagCLabel.setBounds(470,135, 320,15);
         this.stateFrame.add(flagCLabel);
 
         Label flagVLabel = new Label("V: " + (flags[3] ? "1" : "0"));
-        flagVLabel.setBounds(470,140, 320,15);
+        flagVLabel.setBounds(470,150, 320,15);
         this.stateFrame.add(flagVLabel);
 
         Button stepOne = new Button("Step One Instruction");
-        stepOne.setBounds(470,160,300,30);
+        stepOne.setBounds(470,170,300,30);
         stepOne.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 simulator.executeInstruction();
+                if (simulator.getCurrentInstruction().getMnemonic().equals("HALT")) {
+                    if (timer != null) timer.stop();
+                    simulator.writeToFile();
+                }
             }
         });
         this.stateFrame.add(stepOne);
+
+        Button fastButton = new Button("Run Fast");
+        fastButton.setBounds(480,210,80,30);
+        fastButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                ActionListener interval = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                        simulator.executeInstruction();
+                        if (simulator.getCurrentInstruction().getMnemonic().equals("HALT")) {
+                            if (timer != null) timer.stop();
+                            simulator.writeToFile();
+                        }
+                    }
+                };
+                timer = new Timer(250, interval);
+                timer.start();
+            }
+        });
+        this.stateFrame.add(fastButton);
+
+        Button mediumButton = new Button("Run Medium");
+        mediumButton.setBounds(570,210,100,30);
+        mediumButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                ActionListener interval = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                        simulator.executeInstruction();
+                        if (simulator.getCurrentInstruction().getMnemonic().equals("HALT")) {
+                            if (timer != null) timer.stop();
+                            simulator.writeToFile();
+                        }
+                    }
+                };
+                timer = new Timer(500, interval);
+                timer.start();
+            }
+        });
+        this.stateFrame.add(mediumButton);
+
+        Button slowButton = new Button("Run Slow");
+        slowButton.setBounds(680,210,80,30);
+        slowButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                ActionListener interval = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                        simulator.executeInstruction();
+                        if (simulator.getCurrentInstruction().getMnemonic().equals("HALT")) {
+                            if (timer != null) timer.stop();
+                            simulator.writeToFile();
+                        }
+                    }
+                };
+                timer = new Timer(1000, interval);
+                timer.start();
+            }
+        });
+        this.stateFrame.add(slowButton);
+
+        Button stopButton = new Button("Stop");
+        stopButton.setBounds(480,250,80,30);
+        stopButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                if (timer != null) timer.stop();
+            }
+        });
+        this.stateFrame.add(stopButton);
+
+        Button resetButton = new Button("Reset");
+        resetButton.setBounds(680,250,80,30);
+        resetButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                if (timer != null) timer.stop();
+                simulator.reset();
+            }
+        });
+        this.stateFrame.add(resetButton);
+
+        Button writeButton = new Button("Write to Image File");
+        writeButton.setBounds(470,290,300,30);
+        writeButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                simulator.writeToFile();
+            }
+        });
+        this.stateFrame.add(writeButton);
     }
 
-    public void showMemory(Byte[] memory) {
+    private void showMemory(Byte[] memory) {
+        this.memFrame.removeAll();
 
+        Label memLabel = new Label("Memory:");
+        memLabel.setBounds(10, 10, 100, 20);
+        this.memFrame.add(memLabel);
+
+        String memData = "";
+        for (int i = 0; i < memory.length; i++) {
+            String binary = Integer.toBinaryString(Integer.parseInt(memory[i].toString()));
+            while (binary.length() < 8) binary = "0" + binary;
+            if (binary.length() > 8) binary = binary.substring(binary.length() - 8);
+            memData = memData + "0x" +  Integer.toHexString(i) + ":\t" + binary + "\n";
+        }
+        TextArea memoryDisplay = new TextArea(memData);
+        memoryDisplay.setBounds(10,40, 380,750);
+        this.memFrame.add(memoryDisplay);
     }
 }
